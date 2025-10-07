7c5a49550c642ea8b3e864fcf048057484406fa7
lazybonez2006 <lazybonez2006@gmail.com>
2025-10-07 17:26:35 -0400

    damage script

---
+++ Deserted Blood/Assets/Scripts/Damage.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] int damageammount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       Idamage dmg = other.GetComponent<Idamage>();
+        if(dmg != null)
+        {
+            dmg.TakeDamage(damageammount);
+        }
+    }
+}
