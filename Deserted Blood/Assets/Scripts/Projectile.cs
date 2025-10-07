9aa024c4221a243630e7dee4cfb14a8b7db5b877
lazybonez2006 <lazybonez2006@gmail.com>
2025-10-07 17:45:42 -0400

    projectile script

--- Deserted Blood/Assets/Scripts/Projectile.cs
+++ Deserted Blood/Assets/Scripts/Projectile.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float destroytime;
+    [SerializeField] Rigidbody rb;
+    public Damage dmg;
+    private void Awake()
+    {
+        dmg=GetComponent<Damage>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject, destroytime);
+        rb.linearVelocity = transform.forward * speed;
     }
 
     // Update is called once per frame
