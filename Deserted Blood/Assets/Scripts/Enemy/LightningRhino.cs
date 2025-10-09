b4f17d0f23b0a5e4ed6681a0c4b04fefaa1e70ca
Electricfan54 <electivefan54@gmail.com>
2025-10-09 15:48:26 -0400

    Made the lightning rhino enemy and script. I also made a charging particle effect for the rhino.

---
+++ Deserted Blood/Assets/Scripts/Enemy/LightningRhino.cs
@@ -0,0 +1,105 @@
+using TMPro;
+using UnityEngine;
+public class LightningRhino : EnemyAI
+{
+    [Header("Lightning Rhino")]
+    [SerializeField] float chargeTime;
+    [SerializeField] float lightningRange;
+    [SerializeField] ParticleSystem chargeEffect;
+    float chargeTimer;
+    bool isCharging = false;
+
+
+    protected override void Update()
+    {
+        base.Update();
+        Debug.DrawRay(projectileSpawn.position, projectileSpawn.forward * lightningRange);
+        if (!inAttackAnim)
+            attackTimer += Time.deltaTime;
+    }
+
+    protected override void AttackState()
+    {
+        if (inAttackAnim && isCharging)
+        {
+            ChargeAttack();
+        }
+        else if (!inAttackAnim)
+        {
+            if (attackTimer >= attackRate)
+            {
+                attackTimer = 0;
+                inAttackAnim = true;
+                isCharging = true;
+                chargeEffect.Play();
+            }
+        }
+
+    }
+
+    void ChargeAttack()
+    {
+
+
+        if (chargeTimer >= chargeTime)
+        {
+            chargeTimer = 0;
+            isCharging = false;
+
+            chargeEffect.Stop();
+
+            animator.SetTrigger("Attack0");
+        }
+        else
+        {
+            chargeTimer += Time.deltaTime;
+        }
+
+    }
+
+    public override void RangedAttack0()
+    {
+        Vector3 offsetPos = new Vector3(targetPoint.x, targetPoint.y + 1.0f, 0);
+        Quaternion rot = Quaternion.LookRotation(offsetPos - projectileSpawn.position);
+        projectileSpawn.rotation = rot;
+
+        RaycastHit hit;
+        if (Physics.Raycast(projectileSpawn.position, projectileSpawn.forward, out hit, lightningRange, ~lineOfSightIgnoreLayer))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                hit.collider.GetComponent<Idamage>().TakeDamage(projDamage);
+            }
+        }
+    }
+
+    protected override void HitReact()
+    {
+        base.HitReact();
+        if (isCharging)
+        {
+            isCharging = false;
+            chargeTimer = 0;
+            chargeEffect.Stop();
+        }
+    }
+
+    protected override void AttackTransitionCheck()
+    {
+        if (inAttackAnim && !isCharging)
+            return;
+
+        if (DistFromTarget() > chaseStopDist + 0.1f)//0.1 is a slight offset to prevent constant state changes
+        {
+
+            if (isCharging)
+            {
+                isCharging = false;
+                chargeTimer = 0;
+                chargeEffect.Stop();
+                inAttackAnim = false;
+            }
+            curState = EnemyState.chase;
+        }
+    }
+}
