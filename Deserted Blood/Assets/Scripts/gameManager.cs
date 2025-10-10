40ba4898b3dfde6eabd12380346e222322ed8f85
Jose <149850498+DevPixil1@users.noreply.github.com>
2025-10-10 17:10:19 -0400

    Fixed health calculating with ints

--- Deserted Blood/Assets/Scripts/gameManager.cs
+++ Deserted Blood/Assets/Scripts/gameManager.cs
@@ -345,7 +345,7 @@ public class gameManager : MonoBehaviour
 
     public void UpdateBossBar(int currHealth)
     {
-        bossBar.fillAmount = currHealth / bossHPMax;
+        bossBar.fillAmount = (float)currHealth / (float)bossHPMax;
     }
 
 }
