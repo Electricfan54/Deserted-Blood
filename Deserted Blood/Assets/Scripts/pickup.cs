3c7701e7a98c596db6cece4406ec8a092ea789ea
Gabriel <gabriel.m.alicea@gmail.com>
2025-10-10 20:52:51 -0400

    Syphon abilties

--- Deserted Blood/Assets/Scripts/pickup.cs
+++ Deserted Blood/Assets/Scripts/pickup.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class pickup : MonoBehaviour
 {
+
     [SerializeField] Ability ability;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         IPickup pickup = other.GetComponent<IPickup>();
         if (pickup!=null)
         {
-            ability.currentcharge = ability.maxCharge;
-            pickup.abilitystats(ability);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ability.currentcharge = ability.maxCharge;
+                pickup.abilitystats(ability);
 
-            Destroy(gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 }   
