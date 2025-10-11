f17efbb0c4056ec96f3dcb0fbd09bbef2b6dc1ab
Electricfan54 <electivefan54@gmail.com>
2025-10-11 16:41:48 -0400

    Fixed a potential bug int the player freeze method.

--- Deserted Blood/Assets/Scripts/PlayerController.cs
+++ Deserted Blood/Assets/Scripts/PlayerController.cs
@@ -294,13 +294,19 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
 
         if (meshRenderer.material.color != Color.blue)
         {
-            // Uncomment if player damage flash is implemented
-            //if (meshRenderer.material.color == Color.red)
-            //{
-            //    beforeFreezeColor = origColor;
-            //    origColor = Color.blue;
-            //}
-            //else
+            //Uncomment if player damage flash is implemented
+            /*if (meshRenderer.material.color == Color.red)
+            {
+                beforeFreezeColor = origColor;
+                origColor = Color.blue;
+            }
+            else*/
+            if (meshRenderer.material.color == Color.yellow)
+            {
+                beforeFreezeColor = beforeStunColor;
+                beforeStunColor = Color.blue;
+            }
+            else
                 beforeFreezeColor = meshRenderer.material.color;
             meshRenderer.material.color = Color.blue;
         }
