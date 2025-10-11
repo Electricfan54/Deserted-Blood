dff272baac4a372370483bb5405723951936b83d
Gabriel <gabriel.m.alicea@gmail.com>
2025-10-11 19:39:36 -0400

    Blood to health regen and player prefab update

--- Deserted Blood/Assets/Scripts/PlayerController.cs
+++ Deserted Blood/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,5 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
@@ -30,6 +28,8 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
     int JumpCount;
     Vector3 MoveDirection;
     Vector3 playerVel;
+
+    float RegenHealthTimer = 0;
 
     int CurrentMoveAnimationIndex;
     
@@ -419,6 +419,8 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
 
             StartCoroutine(BasicAttack());
         }
+
+        RegenHealth();
     }
 
     IEnumerator MappaPunch()
@@ -468,5 +470,17 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
         CurrentMoveAnimationIndex += 1;
     }
 
+    void RegenHealth()
+    {
+        RegenHealthTimer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.H) && BloodMeter > 0 && RegenHealthTimer > 0.2f)
+        {
+            RegenHealthTimer = 0;
+            BloodMeter--;
+            HP++;
+            gameManager.instance.UpdateHPBar(MaxHP, HP);
+            gameManager.instance.UpdateBloodMeter(MaxBloodMeter, BloodMeter);
+        }
+    }
 
 }
