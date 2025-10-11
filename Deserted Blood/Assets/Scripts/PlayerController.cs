9e7871ab42a91347cbd157b3bc4d6ae6def3a7ff
Gabriel <gabriel.m.alicea@gmail.com>
2025-10-11 16:36:24 -0400

    Added inviciblity to base ability moves

--- Deserted Blood/Assets/Scripts/PlayerController.cs
+++ Deserted Blood/Assets/Scripts/PlayerController.cs
@@ -399,6 +399,7 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
     {
         canMove = false;
         isAttacking = true;
+        isInvinc = true;
         playerVel.x = transform.forward.x * 20;
         BaseAttacks[0].SetActive(true);
         yield return new WaitForSeconds(.4f);
@@ -406,11 +407,13 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
         playerVel.x = 0;
         isAttacking = false;
         canMove = true;
+        isInvinc = false;
     }
 
     IEnumerator FallingPunch()
     {
         isAttacking = true;
+        isInvinc = true;
         playerVel = new Vector3(transform.forward.x * 10, JumpStrength * 1.2f, 0);
         yield return new WaitForSeconds(.5f);
         BaseAttacks[1].SetActive(true);
@@ -419,6 +422,7 @@ public class PlayerController : MonoBehaviour, Idamage,IPickup, IEffect
         playerVel.x = 0;
         BaseAttacks[1].SetActive(false);
         isAttacking = false;
+        isInvinc = false;
 
     }
 
