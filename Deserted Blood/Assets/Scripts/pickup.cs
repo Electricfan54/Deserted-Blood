54637aaaa231fbe0309d676dd1b1bb6931dfe26f
lazybonez2006 <lazybonez2006@gmail.com>
2025-10-17 11:49:34 -0400

    ability falling and tutorial level

--- Deserted Blood/Assets/Scripts/pickup.cs
+++ Deserted Blood/Assets/Scripts/pickup.cs
@@ -5,9 +5,15 @@ public class pickup : MonoBehaviour
 
     [SerializeField] Ability ability;
     bool canPickup;
-
+    bool isgrounded=false;
+    [SerializeField] LayerMask ground;
     private void Update()
     {
+        if(isgrounded==false)
+        {
+            groundcheck();
+        }
+
         if (canPickup)
         {
             if (Input.GetKeyDown(KeyCode.E))
@@ -44,6 +50,20 @@ public class pickup : MonoBehaviour
         {
             canPickup = false;
             gameManager.instance.pickUpPrompt.SetActive(false);
+        }
+    }
+
+    void groundcheck()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f, ground))
+        {
+            isgrounded = true;
+        }
+        else
+        {
+            gameObject.transform.position = transform.position+Vector3.down*0.1f;
         }
     }
 
