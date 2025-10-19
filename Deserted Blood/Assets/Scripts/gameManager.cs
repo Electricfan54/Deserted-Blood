2566d77c1b4afbffadf90cdd3ea552db6f53e98f
Jose <149850498+DevPixil1@users.noreply.github.com>
2025-10-18 21:43:54 -0400

    Ability slot icons now render properly

--- Deserted Blood/Assets/Scripts/gameManager.cs
+++ Deserted Blood/Assets/Scripts/gameManager.cs
@@ -385,7 +385,7 @@ public class gameManager : MonoBehaviour
         AbilitySlotMain selSlot = abilitySlots[slotSelected];
 
         Ability ability = playerScript.abilities[slotSelected];
-
+        selSlot.abilityIcon.sprite = ability.abilityIcon;
         if (selSlot.chargeList.Count > 0)
         {
             for (int i = 0; i < selSlot.maxCharges; i++)
