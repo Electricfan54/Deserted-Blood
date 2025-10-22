e85428054035aa9e4e72fb6220bcdef5acd47a49
Electricfan54 <electivefan54@gmail.com>
2025-10-22 17:57:05 -0400

    Bosses no longer throw a null error when the player respawns.

--- Deserted Blood/Assets/Scripts/Enemy/Bosses/GateKeeperAI.cs
+++ Deserted Blood/Assets/Scripts/Enemy/Bosses/GateKeeperAI.cs
@@ -361,7 +361,9 @@ public class GateKeeperAI : EnemyAI
     public void StopBossFight()
     {
         gameManager.instance.HideBossBar();
-        gameManager.instance.cameraScript.resetCam();
+        CameraController cam = Camera.main.GetComponent<CameraController>();
+        if (cam != null)
+            cam.resetCam();
         curHealth = maxHealth;
         transform.position = startPos;
         bossFightTriggered = false;
